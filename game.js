9cbda1c865fc2f7ea2685f32f5058f252b227e86
Brady <jackmickermen@gmail.com>
2026-01-14 00:41:18 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-
+const games = [
+    { title: "Retro Bowl", id: "game1", url: "https://class1811.github.io/g5/class-400/" },
+    { title: "Slope", id: "game2", url: "https://class1811.github.io/g5/class-450/" },
+    { title: "Cookie Clicker", id: "game3", url: "https://class1811.github.io/g5/class-448/" },
+    { title: "Geometry Dash Lite", id: "game4", url: "https://geometrylitegame.org/game/geometry-dash-lite/" }
+];
