9ded26e203eecffac6968a5c79858c7ee3ff41ae
Brady <jackmickermen@gmail.com>
2026-01-15 13:28:06 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,3 @@
-const games = [
-    { title: "Retro Bowl", url: "/games/retrobowl/index.html", image: "https://classroom2111.github.io/img/class-400.png" },
-    { title: "Slope", url: "/games/slope/index.html", image: "https://classroom2111.github.io/img/class-450.png" },
-    { title: "Geometry Dash", url: "/games/geometrydash/index.html", image: "https://classroom2111.github.io/img/class-453.png" },
-    // add more games here
-];
-
-const container = document.getElementById('gameContainer');
-
-function escapeHtml(str) {
-    return str.replace(/&/g,'&amp;')
-              .replace(/</g,'&lt;')
-              .replace(/>/g,'&gt;')
-              .replace(/"/g,'&quot;');
-}
-
-function createCardHTML(g) {
-    return `
-        <div class="game-card" data-title="${escapeHtml(g.title)}">
-            <a href="${g.url}">
-                <div class="game-box">
-                    <img class="game-image" src="${g.image}" alt="${escapeHtml(g.title)}">
-                    <div class="game-title">${escapeHtml(g.title)}</div>
-                </div>
-            </a>
-        </div>
-    `;
-}
-
-games.forEach(g => container.insertAdjacentHTML('beforeend', createCardHTML(g)));
 
 function searchGames() {
     const q = document.getElementById('searchInput').value.trim().toLowerCase();
